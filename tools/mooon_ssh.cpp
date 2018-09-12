9332f18e0c80ccf9e664f7a27fc52ff680dc5dd1
eyjian <eyjian@live.com>
2018-09-12 20:14:19 +0800

    支持并行执行

--- tools/mooon_ssh.cpp
+++ tools/mooon_ssh.cpp
@@ -24,17 +24,21 @@
 // 使用示例（-p指定端口，-P指定密码）：
 // mooon_ssh -u=root -P=test -p=2016 -h="127.0.0.1,192.168.0.1" -c='ls /tmp&&ps aux|grep -c test'
 //
+// 可通过为参数“-thr”指定大于1的值并发执行
+//
 // 可环境变量HOSTS替代参数“-h”
 // 可环境变量USER替代参数“-u”
 // 可环境变量PASSWORD替代参数“-p”
 #include "mooon/net/libssh2.h" // 提供远程执行命令接口
 #include "mooon/sys/error.h"
 #include "mooon/sys/stop_watch.h"
+#include "mooon/sys/thread_engine.h"
 #include "mooon/utils/args_parser.h"
 #include "mooon/utils/print_color.h"
 #include "mooon/utils/string_utils.h"
 #include "mooon/utils/tokener.h"
 #include <iostream>
+#include <sstream>
 
 // 逗号分隔的远程主机IP列表
 STRING_ARG_DEFINE(h, "", "Connect to the remote machines on the given hosts separated by comma, can be replaced by environment variable 'H', example: -h='192.168.1.10,192.168.1.11'");
@@ -56,8 +60,8 @@ STRING_ARG_DEFINE(c, "", "The command is executed on the remote machines, exampl
 // 2 只输出总结
 INTEGER_ARG_DEFINE(uint8_t, v, 1, 0, 2, "Verbosity, how much troubleshooting info to print");
 
-// 线程数（parallel）
-INTEGER_ARG_DEFINE(int, threads, 0, 0, 100000, "The number of threads");
+// 线程数（parallel），多线程并行执行
+INTEGER_ARG_DEFINE(int, thr, 0, 0, 2018, "The number of threads to parallel execute, can be replaced by environment variable 'THR'");
 
 // 结果信息
 struct ResultInfo
@@ -85,7 +89,33 @@ inline std::ostream& operator <<(std::ostream& out, const struct ResultInfo& res
     return out;
 }
 
-static void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands);
+// 取得线程数
+static int get_num_of_threads();
+
+// thread 是否运行在线程中
+static void mooon_ssh(bool thread, struct ResultInfo& result, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands);
+
+struct SshTask
+{
+    struct ResultInfo* result;
+    std::string remote_host_ip;
+    int port;
+    std::string user;
+    std::string password;
+    std::string commands;
+};
+
+class CSShThread
+{
+public:
+    CSShThread(int index) { _index = index; }
+    void add_task(struct ResultInfo& result, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands);
+    void run();
+
+private:
+    int _index;
+    std::vector<struct SshTask> _tasks;
+};
 
 // 使用示例：
 // mooon_ssh -u=root -P=test -p=2016 -h="127.0.0.1,192.168.0.1" -c='ls /tmp&&ps aux|grep -c test'
@@ -203,6 +233,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    time_t start_seconds = time(NULL);
     std::vector<std::string> hosts_ip;
     const std::string& remote_hosts_ip = hosts;
     const int num_remote_hosts_ip = mooon::utils::CTokener::split(&hosts_ip, remote_hosts_ip, ",", true);
@@ -213,6 +244,18 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
+    // 先创建线程对象，但不要启动线程
+    const int num_threads = get_num_of_threads();
+    std::vector<CSShThread*> ssh_threads;
+    if (num_threads > 1)
+    {
+        for (int i=0; i<num_threads; ++i)
+        {
+            CSShThread* ssh_thread = new CSShThread(i);
+            ssh_threads.push_back(ssh_thread);
+        }
+    }
+
     mooon::net::CLibssh2::init();
     std::vector<struct ResultInfo> results(num_remote_hosts_ip);
     for (int i=0; i<num_remote_hosts_ip; ++i)
@@ -220,8 +263,37 @@ int main(int argc, char* argv[])
         struct ResultInfo& result = results[i];
         const std::string& remote_host_ip = hosts_ip[i];
 
-        ssh_proc(result, i, remote_host_ip, port, user, password, commands);
+        if (num_threads <= 1)
+        {
+            mooon_ssh(false, result, remote_host_ip, port, user, password, commands);
+        }
+        else
+        {
+            CSShThread* ssh_thread = ssh_threads[i % num_threads];
+            ssh_thread->add_task(result, remote_host_ip, port, user, password, commands);
+        }
     } // for
+
+    // 启动所有线程
+    if (num_threads > 1)
+    {
+        std::vector<mooon::sys::CThreadEngine*> thread_engines(num_threads);
+        for (int i=0; i<num_threads; ++i)
+        {
+            CSShThread* ssh_thread = ssh_threads[i];
+            thread_engines[i] = new mooon::sys::CThreadEngine(mooon::sys::bind(&CSShThread::run, ssh_thread));
+        }
+
+        for (int i=0; i<num_threads; ++i)
+        {
+            thread_engines[i]->join();
+            delete ssh_threads[i];
+            delete thread_engines[i];
+        }
+
+        thread_engines.clear();
+        ssh_threads.clear();
+    }
     mooon::net::CLibssh2::fini();
 
     std::cout << std::endl;
@@ -248,9 +320,15 @@ int main(int argc, char* argv[])
             ++num_failure;
     }
 
+    const time_t end_seconds = time(NULL);
+    int cost_seconds = 0;
+    if (end_seconds > start_seconds)
+    {
+        cost_seconds = static_cast<int>(end_seconds - start_seconds);
+    }
     if ((mooon::argument::v->value() >= 1) && (mooon::argument::v->value() <= 2))
     {
-        std::cout << "SUCCESS: " << num_success << ", FAILURE: " << num_failure << std::endl;
+        std::cout << "SUCCESS: " << num_success << ", FAILURE: " << num_failure << ", SECONDS: "<< cost_seconds<< std::endl;
     }
 #else
     fprintf(stderr, "NOT IMPLEMENT! please install libssh2 (https://www.libssh2.org/) into /usr/local/libssh2 and recompile.\n");
@@ -259,7 +337,21 @@ int main(int argc, char* argv[])
     return (0 == num_failure)? 0: 1;
 }
 
-void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands)
+int get_num_of_threads()
+{
+    int num_threads = mooon::argument::thr->value();
+
+    const char* str = getenv("THR");
+    if (str != NULL)
+    {
+        if (!mooon::utils::CStringUtils::string2int(str, num_threads))
+            num_threads = mooon::argument::thr->value();
+    }
+
+    return num_threads;
+}
+
+void mooon_ssh(bool thread, struct ResultInfo& result, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands)
 {
     bool color = true;
     int num_bytes = 0;
@@ -274,73 +366,91 @@ void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_ho
         str = mooon::utils::CStringUtils::format_string("[" PRINT_COLOR_YELLOW"%s" PRINT_COLOR_NONE"]\n", remote_host_ip.c_str());
         str += PRINT_COLOR_GREEN;
         screen += str;
-        fprintf(stdout, "%s", str.c_str());
+        if (!thread)
+            fprintf(stdout, "%s", str.c_str());
     }
 
     mooon::sys::CStopWatch stop_watch;
     try
     {
+        std::stringstream out;
         mooon::net::CLibssh2 libssh2(remote_host_ip, port, user, password, mooon::argument::t->value());
 
-        libssh2.remotely_execute(commands, std::cout, &exitcode, &exitsignal, &errmsg, &num_bytes);
+        libssh2.remotely_execute(commands, out, &exitcode, &exitsignal, &errmsg, &num_bytes);
         str = PRINT_COLOR_NONE;
         screen += str;
-        fprintf(stdout, "%s", str.c_str());
+        if (!thread)
+            fprintf(stdout, "%s", str.c_str());
         color = false; // color = true;
 
         if ((0 == exitcode) && exitsignal.empty())
         {
             result.success = true;
 
+            if (!thread)
+                fprintf(stdout, "%s", out.str().c_str());
+            else
+                screen += out.str();
             if ((mooon::argument::v->value() >= 1) && (mooon::argument::v->value() <= 1))
             {
                 str = mooon::utils::CStringUtils::format_string("[" PRINT_COLOR_YELLOW"%s" PRINT_COLOR_NONE"] SUCCESS\n", remote_host_ip.c_str());
                 screen += str;
-                fprintf(stdout, "%s", str.c_str());
+                if (!thread)
+                    fprintf(stdout, "%s", str.c_str());
             }
         }
         else
         {
             result.success = false;
 
+            if (!thread)
+                fprintf(stderr, "%s", out.str().c_str());
+            else
+                screen += out.str();
             if (exitcode != 0)
             {
                 if (1 == exitcode)
                 {
                     str = mooon::utils::CStringUtils::format_string("command return %d\n", exitcode);
                     screen += str;
-                    fprintf(stderr, "%s", str.c_str());
+                    if (!thread)
+                        fprintf(stderr, "%s", str.c_str());
                 }
                 else if (126 == exitcode)
                 {
                     str = mooon::utils::CStringUtils::format_string("%d: command not executable\n", exitcode);
                     screen += str;
-                    fprintf(stderr, "%s", str.c_str());
+                    if (!thread)
+                        fprintf(stderr, "%s", str.c_str());
                 }
                 else if (127 == exitcode)
                 {
                     str = mooon::utils::CStringUtils::format_string("%d: command not found\n", exitcode);
                     screen += str;
-                    fprintf(stderr, "%s", str.c_str());
+                    if (!thread)
+                        fprintf(stderr, "%s", str.c_str());
                 }
                 else if (255 == exitcode)
                 {
                     str = mooon::utils::CStringUtils::format_string("%d: command not found\n", 127);
                     screen += str;
-                    fprintf(stderr, "%s", str.c_str());
+                    if (!thread)
+                        fprintf(stderr, "%s", str.c_str());
                 }
                 else
                 {
                     str = mooon::utils::CStringUtils::format_string("%d: %s\n", exitcode, mooon::sys::Error::to_string(exitcode).c_str());
                     screen += str;
-                    fprintf(stderr, "%s", str.c_str());
+                    if (!thread)
+                        fprintf(stderr, "%s", str.c_str());
                 }
             }
             else if (!exitsignal.empty())
             {
                 str = mooon::utils::CStringUtils::format_string("%s: %s\n", exitsignal.c_str(), errmsg.c_str());
                 screen += str;
-                fprintf(stderr, "%s", str.c_str());
+                if (!thread)
+                    fprintf(stderr, "%s", str.c_str());
             }
         }
     }
@@ -352,12 +462,14 @@ void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_ho
             {
                 screen += PRINT_COLOR_NONE; // color = true;
                 screen += str;
-                fprintf(stdout, "%s", str.c_str());
+                if (!thread)
+                    fprintf(stdout, "%s", str.c_str());
             }
 
             str = mooon::utils::CStringUtils::format_string("[" PRINT_COLOR_RED"%s" PRINT_COLOR_NONE"] failed: %s\n", remote_host_ip.c_str(), ex.str().c_str());
             screen += str;
-            fprintf(stderr, "%s", str.c_str());
+            if (!thread)
+                fprintf(stderr, "%s", str.c_str());
         }
     }
     catch (mooon::utils::CException& ex)
@@ -368,12 +480,14 @@ void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_ho
             {
                 str = PRINT_COLOR_NONE; // color = true;
                 screen += str;
-                fprintf(stdout, "%s", str.c_str());
+                if (!thread)
+                    fprintf(stdout, "%s", str.c_str());
             }
 
             str = mooon::utils::CStringUtils::format_string("[" PRINT_COLOR_RED"%s" PRINT_COLOR_NONE"] failed: %s\n", remote_host_ip.c_str(), ex.str().c_str());
             screen += str;
-            fprintf(stderr, "%s", str.c_str());
+            if (!thread)
+                fprintf(stderr, "%s", str.c_str());
         }
     }
 
@@ -382,6 +496,32 @@ void ssh_proc(struct ResultInfo& result, int index, const std::string& remote_ho
     {
         str = "\n";
         screen += str;
-        fprintf(stdout, "%s", str.c_str());
+        if (!thread)
+            fprintf(stdout, "%s", str.c_str());
     }
+    if (thread)
+    {
+        write(STDIN_FILENO, screen.c_str(), screen.size());
+    }
+}
+
+void CSShThread::run()
+{
+    for (std::vector<struct SshTask>::size_type i=0; i<_tasks.size(); ++i)
+    {
+        const struct SshTask& task = _tasks[i];
+        mooon_ssh(true, *task.result, task.remote_host_ip, task.port, task.user, task.password, task.commands);
+    }
+}
+
+void CSShThread::add_task(struct ResultInfo& result, const std::string& remote_host_ip, int port, const std::string& user, const std::string& password, const std::string& commands)
+{
+    struct SshTask task;
+    task.result = &result;
+    task.remote_host_ip = remote_host_ip;
+    task.port = port;
+    task.user = user;
+    task.password = password;
+    task.commands = commands;
+    _tasks.push_back(task);
 }
