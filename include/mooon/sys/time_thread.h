ff0e7f85ef45c21f46e70c11eb3396ecbebabd93
eyjian <eyjian@live.com>
2019-02-27 18:38:08 +0800

    增加类CTimeThread，用于提供精度要求不高的秒级时间服务

---
+++ include/mooon/sys/time_thread.h
@@ -0,0 +1,37 @@
+// Writed by yijian on 2019/2/27
+#ifndef MOOON_SYS_TIME_THREAD_H
+#define MOOON_SYS_TIME_THREAD_H
+#include <mooon/sys/atomic.h>
+#include <mooon/sys/thread_engine.h>
+#include <mooon/sys/utils.h>
+#include <mooon/utils/config.h>
+#include <time.h>
+SYS_NAMESPACE_BEGIN
+
+// 提供秒级时间，
+// 可用于避免多个线程重复调用time(NULL)
+class CTimeThread
+{
+    SINGLETON_DECLARE(CTimeThread);
+
+public:
+    CTimeThread();
+    ~CTimeThread();
+    time_t get_seconds() const;
+    void stop();
+    bool start();
+    void wait();
+    void run();
+
+private:
+    CAtomic<bool> _stop;
+#if __WORDSIZE==64
+    CAtomic<int64_t> _seconds;
+#else
+    CAtomic<int> _seconds;
+#endif // __WORDSIZE==64
+    CThreadEngine* _engine;
+};
+
+SYS_NAMESPACE_END
+#endif // MOOON_SYS_TIME_THREAD_H
---
+++ src/sys/time_thread.cpp
@@ -0,0 +1,83 @@
+// Writed by yijian on 2019/2/27
+#include "sys/time_thread.h"
+#include "sys/log.h"
+SYS_NAMESPACE_BEGIN
+
+SINGLETON_IMPLEMENT(CTimeThread);
+
+CTimeThread::CTimeThread()
+    : _stop(false),
+      _seconds(time(NULL)),
+      _engine(NULL)
+{
+}
+
+CTimeThread::~CTimeThread()
+{
+    wait();
+}
+
+time_t CTimeThread::get_seconds() const
+{
+    const int64_t seconds = _seconds;
+    return static_cast<time_t>(seconds);
+}
+
+void CTimeThread::stop()
+{
+    _stop = true;
+}
+
+bool CTimeThread::start()
+{
+    try
+    {
+        _engine = new mooon::sys::CThreadEngine(mooon::sys::bind(&CTimeThread::run, this));
+        return true;
+    }
+    catch (sys::CSyscallException& ex)
+    {
+        MYLOG_ERROR("Start time-thread failed: %s\n", ex.str().c_str());
+        return false;
+    }
+}
+
+void CTimeThread::wait()
+{
+    if (_engine != NULL)
+    {
+        _engine->join();
+        delete _engine;
+        _engine = NULL;
+    }
+}
+
+void CTimeThread::run()
+{
+    const time_t startseconds = time(NULL);
+    _seconds = startseconds;
+
+    MYLOG_INFO("Time-thread start now\n");
+    while (!_stop)
+    {
+        const time_t seconds = time(NULL);
+#if __WORDSIZE==64
+        _seconds = static_cast<int64_t>(seconds);
+#else
+        _seconds = static_cast<int>(seconds);
+#endif // __WORDSIZE==64
+        CUtils::millisleep(1000);
+    }
+
+    const time_t exitseconds = time(NULL);
+    if (startseconds < exitseconds)
+    {
+        MYLOG_INFO("Time-thread exit now: %us\n", static_cast<unsigned int>(exitseconds-startseconds));
+    }
+    else
+    {
+        MYLOG_INFO("Time-thread exit now\n");
+    }
+}
+
+SYS_NAMESPACE_END
