3a51b8393dda05e8723b4746b953d21b1f29fff4
eyjian <eyjian@live.com>
2019-05-10 16:15:54 +0800

    CRSAHelper支持密钥文件和密钥字符串两种形式的key

--- include/mooon/utils/rsa_helper.h
+++ include/mooon/utils/rsa_helper.h
@@ -24,14 +24,37 @@
 #include <stdint.h>
 UTILS_NAMESPACE_BEGIN
 
-enum PaddingFlag
+// RSA填充模式
+enum RSAPaddingMode
 {
+    // RSAES-PKCS1-v1_5/RSASSA-PKCS1-v1_5填充
+    // PKCS #1 v1.5 padding. This currently is the most widely used mode.
     PKCS1_PADDING = 1,
+
+    // SSLv23填充
+    // PKCS #1 v1.5 padding with an SSL-specific modification that denotes that the server is SSL3 capable.
     SSLV23_PADDING = 2,
-    NO_PADDING = 3
+
+    // 不填充
+    // Raw RSA encryption.
+    // This mode should only be used to implement cryptographically sound padding modes
+    // in the application code.
+    // Encrypting user data directly with RSA is insecure.
+    NO_PADDING = 3,
+
+    // RSAES-OAEP填充，强制使用SHA1（加密使用）
+    // EME-OAEP as defined in PKCS #1 v2.0 with SHA-1 , MGF1 and an empty encoding parameter.
+    // This mode is recommended for all new applications.
+    PKCS1_OAEP_PADDING = 4,
+
+    // X9.31填充（签名使用）
+    X931_PADDING = 5,
+
+    // RSASSA-PSS填充（签名使用）
+    PKCS1_PSS_PADDING = 6
 };
 
-// RSA加密算法是一种非对称加密算法
+// RSA是一种非对称加密算法
 // 1977年由罗纳德·李维斯特（Ron Rivest）、阿迪·萨莫尔（Adi Shamir）和伦纳德·阿德曼（Leonard Adleman）一起提出的
 // RSA是他们三人姓氏开头字母拼在一起组成
 class CRSAHelper
@@ -40,10 +63,21 @@ public:
     // pub_keyfile 存储了公钥的文件
     // priv_keyfile 存储了私钥的文件
     // 如果出错，则抛出异常mooon::utils::CException
-    static void public_encrypt(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag=NO_PADDING);
-    static void private_decrypt(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag=NO_PADDING);
-    static void private_encrypt(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag=NO_PADDING);
-    static void public_decrypt(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag=NO_PADDING);
+    static void public_encrypt_bykeyfile(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_decrypt_bykeyfile(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_encrypt_bykeyfile(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void public_decrypt_bykeyfile(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+
+    static void public_encrypt_bykey(const std::string& pub_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_decrypt_bykey(const std::string& priv_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_encrypt_bykey(const std::string& priv_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void public_decrypt_bykey(const std::string& pub_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+
+private:
+    static void public_encrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_decrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void private_encrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
+    static void public_decrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode);
 };
 
 UTILS_NAMESPACE_END
--- src/utils/rsa_helper.cpp
+++ src/utils/rsa_helper.cpp
@@ -25,12 +25,23 @@
 #include <openssl/pem.h>
 UTILS_NAMESPACE_BEGIN
 
+// 生成密钥文件（包含公钥和私钥两部分）：
+// openssl genrsa -out abc.key 1024
+//
+// 提取公钥：
+// openssl rsa -in abc.key -pubout -out pub.key
+//
+// 用公钥加密文件：
+// openssl rsautl -encrypt -in test.txt -inkey pub.key -pubin -out test.dat
+//
+// 用私钥解密文件：
+// openssl rsautl -decrypt -in test.dat -inkey abc.key -out test.txt
+
 // 参考：
 // http://hayageek.com/rsa-encryption-decryption-openssl-c/
-void CRSAHelper::public_encrypt(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag)
+void CRSAHelper::public_encrypt_bykeyfile(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
 {
     int errcode = 0;
-    int rsalen = 0;
     RSA* rsa = NULL;
     FILE* fp = fopen(pub_keyfile.c_str(), "r");
 
@@ -49,26 +60,23 @@ void CRSAHelper::public_encrypt(const std::string& pub_keyfile, const std::strin
         THROW_EXCEPTION(utils::CStringUtils::format_string("read %s failed: %s", pub_keyfile.c_str(), buf.get()), errcode);
     }
 
-    rsalen = RSA_size(rsa);
-    outstr->resize(rsalen + 1);
-    if (-1 == RSA_public_encrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa, flag))
+    try
     {
-        errcode = ERR_get_error();
-        utils::ScopedArray<char> buf(new char[SIZE_4K]);
-        ERR_error_string_n(errcode, buf.get(), SIZE_4K);
-        fclose(fp);
+        public_encrypt(rsa, instr, outstr, mode);
         RSA_free(rsa);
-        THROW_EXCEPTION(utils::CStringUtils::format_string("encrypt %s failed: %s", pub_keyfile.c_str(), buf.get()), errcode);
+        fclose(fp);
     }
-
-    fclose(fp);
-    RSA_free(rsa);
+    catch (...)
+    {
+        RSA_free(rsa);
+        fclose(fp);
+        throw;
+    }
 }
 
-void CRSAHelper::private_decrypt(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag)
+void CRSAHelper::private_decrypt_bykeyfile(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
 {
     int errcode = 0;
-    int rsalen = 0;
     RSA* rsa = NULL;
     FILE* fp = fopen(priv_keyfile.c_str(), "r");
 
@@ -87,26 +95,23 @@ void CRSAHelper::private_decrypt(const std::string& priv_keyfile, const std::str
         THROW_EXCEPTION(utils::CStringUtils::format_string("read %s failed: %s", priv_keyfile.c_str(), buf.get()), errcode);
     }
 
-    rsalen = RSA_size(rsa);
-    outstr->resize(rsalen + 1);
-    if (-1 == RSA_private_decrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa, flag))
+    try
     {
-        errcode = ERR_get_error();
-        utils::ScopedArray<char> buf(new char[SIZE_4K]);
-        ERR_error_string_n(errcode, buf.get(), SIZE_4K);
-        fclose(fp);
+        private_decrypt(rsa, instr, outstr, mode);
         RSA_free(rsa);
-        THROW_EXCEPTION(utils::CStringUtils::format_string("encrypt %s failed: %s", priv_keyfile.c_str(), buf.get()), errcode);
+        fclose(fp);
     }
-
-    fclose(fp);
-    RSA_free(rsa);
+    catch (...)
+    {
+        RSA_free(rsa);
+        fclose(fp);
+        throw;
+    }
 }
 
-void CRSAHelper::private_encrypt(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag)
+void CRSAHelper::private_encrypt_bykeyfile(const std::string& priv_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
 {
     int errcode = 0;
-    int rsalen = 0;
     RSA* rsa = NULL;
     FILE* fp = fopen(priv_keyfile.c_str(), "r");
 
@@ -125,26 +130,23 @@ void CRSAHelper::private_encrypt(const std::string& priv_keyfile, const std::str
         THROW_EXCEPTION(utils::CStringUtils::format_string("read %s failed: %s", priv_keyfile.c_str(), buf.get()), errcode);
     }
 
-    rsalen = RSA_size(rsa);
-    outstr->resize(rsalen + 1);
-    if (-1 == RSA_private_encrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa, flag))
+    try
     {
-        errcode = ERR_get_error();
-        utils::ScopedArray<char> buf(new char[SIZE_4K]);
-        ERR_error_string_n(errcode, buf.get(), SIZE_4K);
-        fclose(fp);
+        private_encrypt(rsa, instr, outstr, mode);
         RSA_free(rsa);
-        THROW_EXCEPTION(utils::CStringUtils::format_string("encrypt %s failed: %s", priv_keyfile.c_str(), buf.get()), errcode);
+        fclose(fp);
     }
-
-    fclose(fp);
-    RSA_free(rsa);
+    catch (...)
+    {
+        RSA_free(rsa);
+        fclose(fp);
+        throw;
+    }
 }
 
-void CRSAHelper::public_decrypt(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, PaddingFlag flag)
+void CRSAHelper::public_decrypt_bykeyfile(const std::string& pub_keyfile, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
 {
     int errcode = 0;
-    int rsalen = 0;
     RSA* rsa = NULL;
     FILE* fp = fopen(pub_keyfile.c_str(), "r");
 
@@ -163,20 +165,299 @@ void CRSAHelper::public_decrypt(const std::string& pub_keyfile, const std::strin
         THROW_EXCEPTION(utils::CStringUtils::format_string("read %s failed: %s", pub_keyfile.c_str(), buf.get()), errcode);
     }
 
-    rsalen = RSA_size(rsa);
-    outstr->resize(rsalen + 1);
-    if (-1 == RSA_public_decrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa, flag))
+    try
+    {
+        public_decrypt(rsa, instr, outstr, mode);
+        RSA_free(rsa);
+        fclose(fp);
+    }
+    catch (...)
+    {
+        RSA_free(rsa);
+        fclose(fp);
+        throw;
+    }
+}
+
+void CRSAHelper::public_encrypt_bykey(const std::string& pub_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    BIO* keybio = NULL;
+    RSA* rsa = NULL;
+
+    // BIO_s_mem() return the memory BIO method function.
+    // A memory BIO is a source/sink BIO which uses memory for its I/O.
+    // Data written to a memory BIO is stored in a BUF_MEM structure which is
+    // extended as appropriate to accommodate the stored data.
+    //
+    // https://linux.die.net/man/3/bio_new_mem_buf
+    keybio = BIO_new_mem_buf((void*)pub_key.c_str(), -1);
+    if (keybio == NULL)
     {
         errcode = ERR_get_error();
-        utils::ScopedArray<char> buf(new char[SIZE_4K]);
-        ERR_error_string_n(errcode, buf.get(), SIZE_4K);
-        fclose(fp);
-        RSA_free(rsa);
-        THROW_EXCEPTION(utils::CStringUtils::format_string("encrypt %s failed: %s", pub_keyfile.c_str(), buf.get()), errcode);
+        THROW_EXCEPTION("failed to create key BIO", errcode);
     }
 
-    fclose(fp);
-    RSA_free(rsa);
+    try
+    {
+        PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
+        public_encrypt(rsa, instr, outstr, mode);
+        RSA_free(rsa);
+        BIO_free(keybio);
+    }
+    catch (...)
+    {
+        RSA_free(rsa);
+        BIO_free(keybio);
+        throw;
+    }
+}
+
+void CRSAHelper::private_decrypt_bykey(const std::string& priv_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    BIO* keybio = NULL;
+    RSA* rsa = NULL;
+
+    keybio = BIO_new_mem_buf((void*)priv_key.c_str(), -1);
+    if (keybio == NULL)
+    {
+        errcode = ERR_get_error();
+        THROW_EXCEPTION("failed to create key BIO", errcode);
+    }
+
+    try
+    {
+        PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+        private_decrypt(rsa, instr, outstr, mode);
+        RSA_free(rsa);
+        BIO_free(keybio);
+    }
+    catch (...)
+    {
+        RSA_free(rsa);
+        BIO_free(keybio);
+        throw;
+    }
+}
+
+void CRSAHelper::private_encrypt_bykey(const std::string& priv_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    BIO* keybio = NULL;
+    RSA* rsa = NULL;
+
+    keybio = BIO_new_mem_buf((void*)priv_key.c_str(), -1);
+    if (keybio == NULL)
+    {
+        errcode = ERR_get_error();
+        THROW_EXCEPTION("failed to create key BIO", errcode);
+    }
+
+    try
+    {
+        PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+        private_encrypt(rsa, instr, outstr, mode);
+        RSA_free(rsa);
+        BIO_free(keybio);
+    }
+    catch (...)
+    {
+        RSA_free(rsa);
+        BIO_free(keybio);
+        throw;
+    }
+}
+
+void CRSAHelper::public_decrypt_bykey(const std::string& pub_key, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    BIO* keybio = NULL;
+    RSA* rsa = NULL;
+
+    keybio = BIO_new_mem_buf((void*)pub_key.c_str(), -1);
+    if (keybio == NULL)
+    {
+        errcode = ERR_get_error();
+        THROW_EXCEPTION("failed to create key BIO", errcode);
+    }
+
+    try
+    {
+        PEM_read_bio_RSA_PUBKEY(keybio, &rsa, NULL, NULL);
+        public_decrypt(rsa, instr, outstr, mode);
+        RSA_free(rsa);
+        BIO_free(keybio);
+    }
+    catch (...)
+    {
+        RSA_free(rsa);
+        BIO_free(keybio);
+        throw;
+    }
+}
+
+void CRSAHelper::CRSAHelper::public_encrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    int rsalen = 0;
+    int outlen = 0;
+    RSA* rsa_ = (RSA*)rsa;
+
+    // Returns the RSA modulus size in bytes.
+    // It can be used to determine how much memory must be allocated for an RSA encrypted value.
+    // rsa->n must not be NULL.
+    // RSA_size() is available in all versions of SSLeay and OpenSSL.
+    rsalen = RSA_size(rsa_);
+    outstr->resize(rsalen);
+    if (mode == PKCS1_PADDING)
+        rsalen = rsalen - 11;
+    else if (mode == PKCS1_OAEP_PADDING)
+        rsalen = rsalen - 41;
+
+    // RSA_public_encrypt() encrypts the flen bytes at
+    // from (usually a session key) using the public key rsa and stores the ciphertext in to.
+    // to must point to RSA_size(rsa) bytes of memory.
+    //
+    // flen must be less than RSA_size(rsa) - 11 for the PKCS #1 v1.5 based padding modes,
+    // less than RSA_size(rsa) - 41 for RSA_PKCS1_OAEP_PADDING and exactly RSA_size(rsa) for RSA_NO_PADDING.
+    // The random number generator must be seeded prior to calling RSA_public_encrypt().
+    //
+    // RSA_public_encrypt() returns the size of the encrypted data (i.e., RSA_size(rsa)).
+    // On error, -1 is returned; the error codes can be obtained by err_get_error(3).
+    //
+    // https://linux.die.net/man/3/rsa_public_encrypt
+    //
+    // int RSA_public_encrypt(int flen, unsigned char *from, unsigned char *to, RSA *rsa, int padding);
+    outlen = RSA_public_encrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa_, mode);
+    if (outlen != -1)
+    {
+        // SUCCESS
+        outstr->resize(outlen);
+    }
+    else
+    {
+        errcode = ERR_get_error();
+        utils::ScopedArray<char> errmsg(new char[SIZE_4K]);
+        ERR_error_string_n(errcode, errmsg.get(), SIZE_4K);
+        THROW_EXCEPTION(errmsg.get(), errcode);
+    }
+}
+
+void CRSAHelper::private_decrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    int rsalen = 0;
+    int outlen = 0;
+    RSA* rsa_ = (RSA*)rsa;
+
+    // Returns the RSA modulus size in bytes.
+    // It can be used to determine how much memory must be allocated for an RSA encrypted value.
+    // rsa->n must not be NULL.
+    // RSA_size() is available in all versions of SSLeay and OpenSSL.
+    rsalen = RSA_size(rsa_);
+    outstr->resize(rsalen);
+    if (mode == PKCS1_PADDING)
+        rsalen = rsalen - 11;
+    else if (mode == PKCS1_OAEP_PADDING)
+        rsalen = rsalen - 41;
+
+    // RSA_private_decrypt() decrypts the flen bytes at from using the private key rsa and stores the plaintext in to.
+    // to must point to a memory section large enough to hold the decrypted data (which is smaller than RSA_size(rsa)).
+    // padding is the padding mode that was used to encrypt the data.
+    //
+    // RSA_private_decrypt() returns the size of the recovered plaintext.
+    // On error, -1 is returned; the error codes can be obtained by err_get_error(3).
+    outlen = RSA_private_decrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa_, mode);
+    if (outlen != -1)
+    {
+        // SUCCESS
+        outstr->resize(outlen);
+    }
+    else
+    {
+        errcode = ERR_get_error();
+        utils::ScopedArray<char> errmsg(new char[SIZE_4K]);
+        ERR_error_string_n(errcode, errmsg.get(), SIZE_4K);
+        THROW_EXCEPTION(errmsg.get(), errcode);
+    }
+}
+
+void CRSAHelper::private_encrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    int rsalen = 0;
+    int outlen = 0;
+    RSA* rsa_ = (RSA*)rsa;
+
+    // Returns the RSA modulus size in bytes.
+    // It can be used to determine how much memory must be allocated for an RSA encrypted value.
+    // rsa->n must not be NULL.
+    // RSA_size() is available in all versions of SSLeay and OpenSSL.
+    rsalen = RSA_size(rsa_);
+    outstr->resize(rsalen);
+    if (mode == PKCS1_PADDING)
+        rsalen = rsalen - 11;
+    else if (mode == PKCS1_OAEP_PADDING)
+        rsalen = rsalen - 41;
+
+    // RSA_private_encrypt() signs the flen bytes at from (usually a message digest with an algorithm identifier)
+    // using the private key rsa and stores the signature in to.
+    // to must point to RSA_size(rsa) bytes of memory.
+    //
+    // RSA_private_encrypt() returns the size of the signature (i.e., RSA_size(rsa)).
+    // On error, -1 is returned; the error codes can be obtained by err_get_error(3).
+    outlen = RSA_private_encrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa_, mode);
+    if (outlen != -1)
+    {
+        outstr->resize(outlen);
+    }
+    else
+    {
+        errcode = ERR_get_error();
+        utils::ScopedArray<char> errmsg(new char[SIZE_4K]);
+        ERR_error_string_n(errcode, errmsg.get(), SIZE_4K);
+        THROW_EXCEPTION(errmsg.get(), errcode);
+    }
+}
+
+void CRSAHelper::public_decrypt(void* rsa, const std::string& instr, std::string* outstr, RSAPaddingMode mode)
+{
+    int errcode = 0;
+    int rsalen = 0;
+    int outlen = 0;
+    RSA* rsa_ = (RSA*)rsa;
+
+    // Returns the RSA modulus size in bytes.
+    // It can be used to determine how much memory must be allocated for an RSA encrypted value.
+    // rsa->n must not be NULL.
+    // RSA_size() is available in all versions of SSLeay and OpenSSL.
+    rsalen = RSA_size(rsa_);
+    outstr->resize(rsalen);
+    if (mode == PKCS1_PADDING) // RSA_PKCS1_PADDING
+        rsalen = rsalen - 11;
+    else if (mode == PKCS1_OAEP_PADDING) // RSA_PKCS1_OAEP_PADDING
+        rsalen = rsalen - 41;
+
+    // RSA_public_decrypt() recovers the message digest from the flen bytes long signature at
+    // from using the signer's public key rsa. to must point to a memory section large enough to
+    // hold the message digest (which is smaller than RSA_size(rsa) - 11).
+    // padding is the padding mode that was used to sign the data.
+    //
+    // RSA_public_decrypt() returns the size of the recovered message digest.
+    // On error, -1 is returned; the error codes can be obtained by err_get_error(3).
+    outlen = RSA_public_decrypt(rsalen, (unsigned char *)instr.data(), (unsigned char*)outstr->data(), rsa_, mode);
+    if (outlen != -1)
+    {
+        outstr->resize(outlen);
+    }
+    else
+    {
+        errcode = ERR_get_error();
+        utils::ScopedArray<char> errmsg(new char[SIZE_4K]);
+        ERR_error_string_n(errcode, errmsg.get(), SIZE_4K);
+        THROW_EXCEPTION(errmsg.get(), errcode);
+    }
 }
 
 UTILS_NAMESPACE_END
