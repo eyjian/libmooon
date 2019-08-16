bcf82f69ca77b71fa30e8fb33f861de56f50f76c
eyjian <eyjian@live.com>
2019-08-16 11:19:58 +0800

    补充有关对内存泄漏判定方法说明

--- shell/mem.sh
+++ shell/mem.sh
@@ -2,6 +2,15 @@
 # writed by yijian on 2016/7/11
 # filename mem.sh
 # 用于统计指定进程的虚拟内存和物理内存占用，以分析是否有内存泄漏
+#
+# 如何判断是否有内存泄漏？
+# 不能只看物理内存或虚拟内存其中一项数据，
+# 如果只是物理内存上涨或只是虚拟内存上涨，
+# 则不能判定为内存泄漏。
+# 当然实际上物理内存不可能超过虚拟内存，
+# 所以内存泄漏必要是虚拟内存和物理内存都会上涨。
+# 如果一段时间内，只是虚拟内存或物理内存一项数据上涨，
+# 则还看不出内存泄漏，可观察更长时间。
 
 if test $# -lt 1; then
     echo "uage: mem.sh pid interval(seconds)"
@@ -20,6 +29,9 @@ pid=$1
 file=$pid.mem
 rm -f $file
 
+# VSS 虚拟内存
+# RSS 物理内存
+echo "                      VSS  RSS"
 while true
 do
     filesize=$(ls -l /tmp/$pid.mem 2>/dev/null|cut -d' ' -f5)
