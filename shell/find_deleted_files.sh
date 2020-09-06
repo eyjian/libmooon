714bf8c7c590aa825dcd7eaefcd1aa26d7b9da6e
eyjian <eyjian@live.com>
2020-09-07 07:51:47 +0800

    优化

--- shell/find_deleted_files.sh
+++ shell/find_deleted_files.sh
@@ -2,7 +2,7 @@
 # Writed by yijian on 2020/9/6
 # 查找被删除但仍然占据磁盘的文件
 
-dirs=(`ls -l --time-style=long-iso /proc  | awk '{ print $8 }'`)
+dirs=(`ls -l --time-style=long-iso /proc 2>/dev/null | awk '{ print $8 }' 2>/dev/null`)
 for ((i=0; i<${#dirs[@]}; ++i))
 do
   dir=${dirs[i]}
@@ -15,5 +15,5 @@ do
   fi
 
   pid=$dir
-  lsof -p $pid | grep "deleted"
+  lsof -p $pid 2>/dev/null | grep "deleted"
 done
